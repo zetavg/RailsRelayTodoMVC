1fdca043d917f766760d3d823a7440d5379498bd
zetavg <mail@zeta.vg>
2017-07-30 00:48:53 +0800

    apply template to the base react app

--- app/javascript/packs/todo_app/index.js
+++ app/javascript/packs/todo_app/index.js
@@ -3,7 +3,57 @@ import React, { Component } from 'react'
 export default class TodoApp extends Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div className="todoapp-container">
+        <section className="todoapp">
+          <header className="header">
+            <h1>todos</h1>
+            <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+          </header>
+          <section className="main">
+            <input id="toggle-all" className="toggle-all" type="checkbox" />
+            <label htmlFor="toggle-all">Mark all as complete</label>
+            <ul className="todo-list">
+              <li className="completed">
+                <div className="view">
+                  <input className="toggle" type="checkbox" checked readOnly />
+                  <label>Taste JavaScript</label>
+                  <button className="destroy" />
+                </div>
+                <input className="edit" value="Create a TodoMVC template" readOnly />
+              </li>
+              <li>
+                <div className="view">
+                  <input className="toggle" type="checkbox" />
+                  <label>Buy a unicorn</label>
+                  <button className="destroy" />
+                </div>
+                <input className="edit" value="Rule the web" readOnly />
+              </li>
+            </ul>
+          </section>
+          <footer className="footer">
+            <span className="todo-count"><strong>0</strong> item left</span>
+            <ul className="filters">
+              <li>
+                <a className="selected" href="#/">All</a>
+              </li>
+              <li>
+                <a href="#/active">Active</a>
+              </li>
+              <li>
+                <a href="#/completed">Completed</a>
+              </li>
+            </ul>
+            <button className="clear-completed">Clear completed</button>
+          </footer>
+        </section>
+        <footer className="info">
+          <p>Double-click to edit a todo</p>
+          <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+          <p>Created by <a href="http://github.com/zetavg">@zetavg</a></p>
+          <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+        </footer>
+      </div>
     )
   }
 }
